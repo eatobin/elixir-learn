63917cce51f8d64a4dfb151d6dabe544ac1de5dc
Eric Tobin <eatobin@gmail.com>
2017-03-17 17:08:07 -0500

    fizz

--- ch5.exs
+++ ch5.exs
@@ -1,3 +1,12 @@
 list_concat = fn (l1, l2) -> l1 ++ l2 end
+
 sum = fn (x, y, z) -> x + y + z end
+
 pair_tuple_to_list = fn ({t1, t2}) -> [t1, t2] end
+
+fizz_buzz = fn
+  (0, 0, _) -> "FizzBuzz"
+  (0, _, _) -> "Fizz"
+  (_, 0, _) -> "Buzz"
+  (_, _, x) -> x
+end
