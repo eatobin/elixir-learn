dee6ee3ee6497abeac39e1e0d8d2507c3c13c417
Eric Tobin <eatobin@gmail.com>
2017-03-17 22:06:26 -0500

    fizzy works!

--- ch5.exs
+++ ch5.exs
@@ -5,10 +5,12 @@ sum = fn (x, y, z) -> x + y + z end
 pair_tuple_to_list = fn ({t1, t2}) -> [t1, t2] end
 
 fizz_buzz = fn
-  (0, 0, _) -> "FizzBuzz"
-  (0, _, _) -> "Fizz"
-  (_, 0, _) -> "Buzz"
-  (_, _, x) -> x
+  0, 0, _ -> "FizzBuzz"
+  0, _, _ -> "Fizz"
+  _, 0, _ -> "Buzz"
+  _, _, x -> x
 end
 
-fizzy = fn (x) -> fizz_buzz.(rem (x,3), rem (x, 5), 10)
+fizzy = fn x -> fizz_buzz.(rem(x, 3), rem(x, 5), x) end
+
+[fizzy.(10), fizzy.(11), fizzy.(12), fizzy.(13), fizzy.(14), fizzy.(15), fizzy.(16)]
