89ad1f235470996070a28f000891c99e0224c1d2
Eric Tobin <eatobin@gmail.com>
2017-03-25 16:37:14 -0500

    progress

--- ch6.exs
+++ ch6.exs
@@ -26,3 +26,11 @@ defmodule GCD do
   def gcd(x,0), do: x
   def gcd(x,y), do: gcd(y,rem(x,y))
 end
+
+defmodule Guard do
+  def what_is(x) when is_number(x) do
+    IO.puts "#{x} is a number"
+  end
+  def what_is(x) when is_list(x), do: IO.puts "#{inspect(x)} is a list"
+  def what_is(x) when is_atom(x), do: IO.puts "#{x} is an atom"
+end
