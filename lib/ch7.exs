030edc154df45cf161569461c0c346397b0e57f7
Eric Tobin <eatobin@gmail.com>
2017-04-16 15:49:05 -0500

    exs to ex

--- lib/ch7.exs
+++ lib/ch7.exs
@@ -1,4 +1,4 @@
 defmodule MyList do
-  def sum1([]),              do: 0
-  def sum1([ head | tail ]), do: head + sum1(tail)
+  defX sum2([]),              do: 0
+  def sum2([ head | tail ]), do: head + sum1(tail)
 end
