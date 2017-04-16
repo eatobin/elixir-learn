3cd9bc69d8c75dab6922a04b03c50542642417e2
Eric Tobin <eatobin@gmail.com>
2017-04-16 16:37:55 -0500

    ch7 progress

--- lib/ch7.exs
+++ lib/ch7.exs
@@ -1,4 +1,7 @@
 defmodule MyList do
   def sum1([]),              do: 0
   def sum1([ head | tail ]), do: head + sum1(tail)
+
+  def len([]), do: 0
+  def len([_head | tail]), do: 1 + len(tail)
 end
