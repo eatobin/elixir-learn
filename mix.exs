dc208377252c7e59080b139fc0726383d04874a8
Eric Tobin <eatobin@gmail.com>
2021-09-13 16:51:22 -0500

    bump elixir

--- mix.exs
+++ mix.exs
@@ -4,7 +4,7 @@ defmodule ElixirLearn.Mixfile do
   def project do
     [app: :elixir_learn,
      version: "0.1.0",
-     elixir: "~> 1.4",
+     elixir: "~> 1.12",
      build_embedded: Mix.env == :prod,
      start_permanent: Mix.env == :prod,
      deps: deps()]
