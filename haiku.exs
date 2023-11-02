88dc72e59fb43708c37c3d78405d82428200bf40
Eric Tobin <eatobin@gmail.com>
2023-11-02 17:19:35 -0500

    add data files

--- haiku.exs
+++ haiku.exs
@@ -1,23 +1,26 @@
 defmodule Haiku do
   def haiku() do
-    {:ok, contents} = File.read("haiku.txt")
+    {:ok, contents} = File.read("day01.txt")
+
     contents
     |> String.split("\n", trim: true)
     |> Enum.map(&String.to_integer/1)
   end
 
   def large() do
-    {:ok, csv} = File.read("large.csv")
+    {:ok, csv} = File.read("day02.csv")
+
     [_head | tail] =
       csv
-      |> String.trim_trailing
-      |> String.split(", ")
+      |> String.trim_trailing()
+      |> String.split(",")
       |> Enum.map(&String.to_integer/1)
+
     tail
   end
 end
 
-
-# iex(3)> c "haiku.exs"                                                                                           
+# iex(3)> c "haiku.exs"
 # [Haiku]
 # iex(4)> Haiku.haiku
+# iex(9)> r Haiku
